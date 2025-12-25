2a0ac348938a59bf9fc293df755c05b146b7b054
Jaivardhan Bhola <jaivardhan.bhola@gmail.com>
2025-12-25 15:32:36 +0530

    updated config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    strapiUrl: process.env.STRAPI_URL || 'https://admin.neurema.com',
+    strapiUrl: process.env.STRAPI_URL || 'http://localhost:1337',
     userApiToken: process.env.STRAPI_USER_API_TOKEN || '',
     contentApiToken: process.env.STRAPI_CONTENT_API_TOKEN || '',
     port: Number(process.env.PORT) || 3000,
